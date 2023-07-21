01e4627ded4d23768bee40bc10223ed05dd3d90f
Srinaath <74102186+thaanirs@users.noreply.github.com>
2023-07-21 13:34:06 +0530

    Add files via upload

--- API.py
+++ API.py
@@ -29,9 +29,9 @@ model_art = BioGptForCausalLM.from_pretrained("microsoft/biogpt")
 
 print()
 
+special_token = "==+=="
 
-
-# chatHistory = ''
+chatHistory = []
 currentQuery = ''
 
 class bot_item(BaseModel):
@@ -42,14 +42,59 @@ class art_item(BaseModel):
     min_len : Optional[int] = 150
     max_len : Optional[int] = 1024
 
+def find_relevance(old,new):
+    currentQuery = "<|user|>are these two issues connected or related? {} and  {} . If yes then provide a number between 0 to 1 representing percentage of relevance of the two issues<|eos|><|ai|>".format(old,new)
+    response = gen(currentQuery, do_sample=True, max_length=250, temperature=0.2)[0]['generated_text']
+    return response
+
+def find_relevance2(old,new):
+    currentQuery = "<|user|>are these two issues connected or related? {} and  {} . say yes or no<|eos|><|ai|>".format(old,new)
+    response = gen(currentQuery, do_sample=True, max_length=250, temperature=0.2)[0]['generated_text']
+    return {"response":response}
+
 def mainBot(query):
     try:
-        global currentQuery
-        userInput = "<user>{}<|eos|><|ai|>".format(query)
-        # currentQuery+=
+        global currentQuery,special_token,chatHistory
+        userInput = "<|user|>{}<|eos|><|ai|>".format(query)
+        if chatHistory:
+            currentQuery = chatHistory[-1]
+        print("current query bfr is",currentQuery)
+        if query.find(special_token)!=-1:
+            currentQuery=''
+            chatHistory=[]
+            query = query.replace(special_token,"")
         currentQuery += userInput
+        print("current query is",currentQuery)
         response = gen(currentQuery, do_sample=True, max_length=250, temperature=0.2)[0]['generated_text']
-        currentQuery = response
+        print("response is",response)
+        chatHistory.append(response[response.rfind("<|user|>"):])
+        print(chatHistory)
+        response = response[response.rfind("<|ai|>")+len("<|ai|>"):]
+        return {"response":response}
+    except :
+        return {"response":'Sorry i could not understand you'}
+
+def mainBot_2(query):
+    try:
+        global currentQuery,special_token,chatHistory
+        userInput = "<|user|>{}<|eos|><|ai|>".format(query)
+        if chatHistory:
+            currentQuery = chatHistory[-1]
+        print("current query bfr is",currentQuery)
+        # if query.find(special_token)!=-1:
+        #     currentQuery=''
+        #     chatHistory=[]
+        #     query = query.replace(special_token,"")
+        print(find_relevance2(query,currentQuery))
+        if find_relevance2(query,currentQuery) == "No":
+            currentQuery=''
+            chatHistory=[]
+        currentQuery += userInput
+        print("current query is",currentQuery)
+        response = gen(currentQuery, do_sample=True, max_length=250, temperature=0.2)[0]['generated_text']
+        print("response is",response)
+        chatHistory.append(response[response.rfind("<|user|>"):])
+        print(chatHistory)
         response = response[response.rfind("<|ai|>")+len("<|ai|>"):]
         return {"response":response}
     except :
@@ -88,3 +133,12 @@ async def botResponse(item:bot_item):
 async def content(item:art_item):
     # print(item)
     return contentGenerate(item.keyword,item.min_len,item.max_len)
+
+@app.post("/rel/")
+async def Rel(query1:str,query2:str):
+    return find_relevance(query1,query2)
+
+@app.post("/medibot_2.0")
+async def botsResponse(item:bot_item):
+    return mainBot_2(item.query)
+
